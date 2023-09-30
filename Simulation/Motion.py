df79f8d23f4a9ddfc04a1232ec9461e0a78ef48a
thinh <huythinhnguyen@gmail.com>
2023-09-30 11:28:38 -0400

    get the Simulation.Motion ready. test a simple episode with fix control cmd or list of cmds

--- Simulation/Motion.py
+++ Simulation/Motion.py
@@ -1,10 +1,11 @@
+from typing import Any
+from numpy.typing import ArrayLike
 import numpy as np
 import sys
-import warnings
-from . import Setting
 
-DEFAULT_BOT = 'create2'
+from . import Setting 
 
+DEFAULT_BOT = 'Create2'
 
 def wrap_to_pi(a):
     if a > np.pi:
@@ -15,39 +16,41 @@ def wrap_to_pi(a):
 
 
 class State:
-    def __init__(self, pose:'[x, y, yaw] (meter)'=None, kinematic:'[v (m/s), w(rad/s)]'=None, dt=0.5):
-        if type(pose)==np.ndarray:
-            self.pose = pose.reshape(3,)
-        else:
-            self.pose = np.array([0.0, 0.0, 0.0]) if pose == None else np.array(pose)
-        if type(kinematic)==np.ndarray:
-            self.kinematic = kinematic.reshape(2,)
-        else:
-            self.kinematic = np.array([0.0, 0.0]) if kinematic==None else np.array(kinematic)
+    def __init__(self, pose: ArrayLike, kinematic: ArrayLike, dt: float):
+        """ pose: [x, y, yaw] (meter, rad), kinematic: [v (m/s), w(rad/s)]
+            dt = 1/echo_call_rate (s)
+        """
+        self.pose = np.asarray(pose).astype(np.float32).reshape(3,)
+        self.kinematic = np.asarray(kinematic).astype(np.float32).reshape(2,)
+
         self._init_state = np.concatenate((self.pose, self.kinematic))
         self.dt = dt
 
+    def run(self, *args, **kwargs) -> np.ndarray:
+        return self._run(*args, **kwargs)
+    
+    def __call__(self, *args: Any, **kwds: Any) -> np.ndarray:
+        return self.run(*args, **kwds)
 
-    def reset(self, pose:'[x, y, yaw] (meter)'=None, kinematic:'[v (m/s), w(rad/s)]'=None):
-        if type(pose)==np.ndarray:
-            self.pose = pose.reshape(3,)
-        else:
-            self.pose = self._init_state[:3] if pose == None else np.array(pose)
-        if type(kinematic)==np.ndarray:
-            self.kinematic = kinematic.reshape(2,)
-        else:
-            self.kinematic = self._init_state[3:] if kinematic ==  None else np.array(kinematic)
+    def reset(self, pose: ArrayLike=[], kinematic: ArrayLike=[]):
+        """ pose: [x, y, yaw] (meter, rad), kinematic: [v (m/s), w(rad/s)]
+        """
+        if len(pose)>0: self.pose = np.asarray(pose).astype(np.float32).reshape(3,)
+        else: pose = self._init_state[:3]
+        if len(kinematic)>0: self.kinematic = np.asarray(kinematic).astype(np.float32).reshape(2,)
+        else: kinematic = self._init_state[3:]
         self._init_state = np.concatenate((self.pose, self.kinematic))
 
-
-    def update_kinematic(self, kinematic:'[v, w]'=[], new_v:'m/s'=None, new_w:'rad/s'=None):
-        if len(kinematic)>1:
-            self.kinematic = kinematic if type(kinematic)==np.ndarray else np.array(kinematic)
-            return self.kinematic
-        if new_v!=None: self.kinematic[0] = new_v
-        if new_w!=None: self.kinematic[1] = new_w
-        # Maybe add a acceleration limitter here
-
+    def update_kinematic(self, kinematic: ArrayLike = [], *kwargs):
+        """ kinematic: [v (m/s), w(rad/s)]
+        """
+        if len(kinematic)==0: kinematic = [0.,0.]
+        velocity_kwd =  set(kwargs.keys()) & set(['v', 'new_v', 'velocity', 'velo'])
+        angular_kwd = set(kwargs.keys()) & set(['w', 'new_w', 'rotational_rate', 'rot_rate', 'angular_velocity', 'angular_velo'])
+        if velocity_kwd: kinematic[0] = kwargs[velocity_kwd.pop()]
+        if angular_kwd: kinematic[1] = kwargs[angular_kwd.pop()]
+        self.kinematic = np.asarray(kinematic).astype(np.float32).reshape(2,)
+        return self.kinematic
     
     def turning_radius(self):
         if np.abs(self.kinematic[1]) > 1e-6: return self.kinematic[0]/self.kinematic[1]
@@ -84,6 +87,11 @@ class State:
             new_pose = self.pose + translation
         self.pose = new_pose.reshape(3,)
         self.pose[2] = wrap_to_pi(self.pose[2])
+    
+    def _run(self, *args, **kwargs):
+        self.update_kinematic(*args, **kwargs)
+        self.update_pose()
+        return self.pose
 
 
 class GPS:
@@ -119,59 +127,3 @@ class GPS:
         offset = self.bot.heading_offset if self.offset else 0.0
         self.pose_hat[2] = wrap_to_pi(self.pose_hat[2] + offset)
 
-
-class Drive:
-    def __init__(self, mode='create2',custom=False,custom_spec=None, noise=True):
-        if custom:
-            self.bot = Setting.Create2(mode=mode, custom_spec = custom_spec, noise=noise)
-        else:
-            self.bot = Setting.Create2(mode=mode,noise=noise)
-
-        self.kinematic = np.array([0.0, 0.0]) # v, w
-        self.direct = np.array([0.0, 0.0]) # vL, vR
-        self.mode = mode
-
-        
-    def reset(self):
-        self.bot.reset()
-
-        
-    def update_kinematic(self, new_v, new_w):
-        self.kinematic[0] = new_v
-        self.kinematic[1] = new_w
-
-
-    def update_direct(self, new_vL, new_vR):
-        self.direct[0] = new_vL
-        self.direct[1] = new_vR
-
-
-    def kinematic_to_direct(self,int=False, wrapping=True):
-        self.direct[0] = self.kinematic[0] - (self.bot.wheelbase/2) * self.kinematic[1]
-        self.direct[1] = self.kinematic[0] + (self.bot.wheelbase/2) * self.kinematic[1]
-        if int:
-            self.direct = np.round(self.direct,3)
-        self.direct[ np.argwhere(abs(self.direct)<self.bot.min_wheel_speed)] = 0
-        if wrapping:
-            self.direct = np.clip(self.direct, self.bot.wheel_velocity_range[0], self.bot.wheel_velocity_range[1])
-
-
-    def direct_to_kinematic(self):
-        self.kinematic[0] = (self.direct[1] + self.direct[0])/2
-        self.kinematic[1] = (self.direct[1] - self.direct[0])/self.bot.wheelbase
-
-
-    def noisy_direct(self, factors, var):
-        # offset left and right to biasing to one side (depending on the setting)
-        self.direct = factors * self.direct
-        # add gaussian noise
-        self.direct = self.direct + np.sqrt(var) * np.random.randn(2)
-
-        
-    def kinematic_update(self, new_kinematic):
-        self.update_kinematic(new_kinematic[0], new_kinematic[1])
-        self.kinematic_to_direct(int=True)
-        # ADD NOISE TO WHEEL VELOCITIES
-        self.noisy_direct(factors = np.array([self.bot.v_left_factor, self.bot.v_right_factor]),
-                          var = self.bot.wheel_velocity_var)
-        self.direct_to_kinematic()
