7ca084b7da9a5670082bc2cb4ed8e779b4edd97d
Thinh Nguyen <huythinhnguyen@gmail.com>
2023-10-04 10:11:25 -0400

    back up some file

---
+++ Gym/ReactiveSingleFlowerDocking/Environment.py
@@ -0,0 +1,170 @@
+import numpy as np
+import pathlib
+import os
+import sys
+
+from tf_agents.environments import py_environment
+from tf_agents.specs import array_spec
+from tf_agents.trajectories import time_step as ts
+
+REPO_NAME = 'FlowerDockingSensorimotorLoopRL'
+REPO_PATH = os.path.abspath(__file__)
+while os.path.basename(REPO_PATH) != REPO_NAME: REPO_PATH = os.path.dirname(REPO_PATH)
+if REPO_PATH not in sys.path: sys.path.append(REPO_PATH)
+
+
+from Sensors.FlowerEchoSimulator import Spatializer
+from Controller.SimpleController import DiscreteActions1, ContinuousActions1
+#from Control.SensorimotorLoops.BatEcho import AvoidApproach
+from Sensors.FlowerEchoSimulator import Setting as sensorconfig
+#from Control.SensorimotorLoops import Setting as controlconfig
+from Simulation.Motion import State
+
+
+OBSERVATION_SIZE = int(2*sensorconfig.AcousticSetting.RAW_DATA_LENGTH/sensorconfig.CompressionSetting.N_SAMPLE)
+CHIRP_RATE = 40 #Hz
+
+
+class DiscreteActionSimpleFlowerDocking(py_environment.PyEnvironment):
+    def __init__(self):
+        init_pose = np.asarray([0, -2, np.radians(90)])
+        self.objects = np.asarray([[0, 0, np.radians(90)]])
+        self.motion = State(pose = init_pose, dt=1/CHIRP_RATE)
+        self.render = Spatializer.Render()
+        self.controller = DiscreteActions1()
+        self._action_spec = array_spec.BoundedArraySpec(shape=(), dtype=np.int32, minimum=-1, maximum=1, name='action')
+        self._observation_spec = array_spec.BoundedArraySpec(shape=(OBSERVATION_SIZE,), dtype=np.float32, minimum=0, name='observation')
+        self._state = np.zeros(OBSERVATION_SIZE)
+        self._episode_ended = False
+        self.step_count = 0
+    
+    def action_spec(self):
+        return self._action_spec
+    
+    def observation_spec(self):
+        return self._observation_spec
+    
+    def _step(self, action, **kwargs):
+        reward = 0
+        if self._episode_ended:
+            return self._reset()
+        # Open your eyes, see where you are
+        self.render.run(pose=self.motion.pose, objects=self.objects)
+        self._state = np.concatenate((self.render.compress_left, self.render.compress_right))
+        # Move according to action
+        v, omega = self.controller(action)
+        self.motion(v=v, w=omega)
+        # if collection
+        # Check for docking
+        # if See Nothing, End Episode.
+        pass
+
+    def _reset(self, **kwargs):
+        pass
+
+
+class DiscreteAction(py_environment.PyEnvironment):
+    def __init__(self, time_limit, level=0, phase=0, difficulty=0, mode='box', max_level=1, log=False):
+        init_pose = help.spawn_bat(mode, phase)
+        self.locomotion = State(pose = init_pose, dt=1/CHIRP_RATE)
+        self.sensor = Spatializer.Render()
+        self.controller = AvoidApproach()
+        self.objects = help.maze_builder(mode=mode)
+        self.objects = np.asarray([[0, 0, np.radians(90)]])
+        #self.objects = np.vstack((np.asarray([ (help.MAZE_SIZE/2-help.TUNNEL_WIDTH/4),1.,1]).reshape(1,3) , self.objects))
+        self._action_spec=array_spec.BoundedArraySpec(shape=(), dtype=np.int32, minimum=0, maximum=1, name='action')
+        self._observation_spec=array_spec.BoundedArraySpec(shape=(OBSERVATION_SIZE,), dtype=np.float64, minimum=0, name='observation')
+        self.echoes = self.sensor.run(pose=self.locomotion.pose, objects=self.objects)
+        self._state = np.asarray(list(self.echoes.values())).reshape(-1,)
+        self._episode_ended = False
+        self.cache = {'time_limit': time_limit, 'init_level': level, 'phase': phase, 'difficulty': difficulty, 'mode': mode, 'max_level': max_level}
+        self.step_count = 0
+        self.level = level
+        self.log = log
+        if log:
+            self.episode = 0
+            self.status = ''
+            self.records = {}
+        
+
+    def action_spec(self):
+        return self._action_spec
+
+    def observation_spec(self):
+        return self._observation_spec
+
+    
+    def _step(self, action, **kwargs):
+        reward = 0
+        if self._episode_ended:
+            return self._reset()
+        # Open your eyes, see where you are
+        if help.collision_check(self.sensor.cache['inview'], 'plant'):
+            reward += help.reward_function(hit='plant')
+            self._episode_ended = True
+            if self.log: self.status='hit'
+        elif help.collision_check(self.sensor.cache['inview'], 'pole'):
+            food_polar = help.get_closest_from_inview(self.sensor.cache['inview'], 'pole')
+            reward += help.reward_function(hit='pole', food_polar=food_polar)
+            self.level += 1
+        # Move according to action
+        v, omega = self.controller.get_kinematic(self.echoes, approach_factor=action)
+        self.locomotion.update_kinematic(kinematic=[v, omega])
+        self.locomotion.update_pose()
+        self.echoes = self.sensor.run(pose=self.locomotion.pose, objects=self.objects)
+        self._state = np.asarray(list(self.echoes.values())).reshape(-1,)
+        #
+        if self.cache['max_level']==0:
+            if help.out_of_bound(pose=self.locomotion.pose, mode=self.cache['mode']):
+                self._episode_ended = True
+                if self.log: self.status = 'outbound'
+        #
+        self.step_count += 1
+        if self.step_count >= self.cache['time_limit'] or self.level >= self.cache['max_level']:
+            self._episode_ended = True
+            if self.log: self.status = 'success' if reward>=1 else 'hitfood' if reward>0 else 'timeout'
+        if self._episode_ended:
+            return ts.termination(self._state, reward=reward)
+        else:
+            return ts.transition(self._state, reward=reward, discount=1.0)
+
+    
+    def _reset(self, **kwargs):
+        if self.log:
+            print('episode {0} : {1} steps, ({2},{3}) ending pose | '.format( self.episode, self.step_count, *np.round(self.locomotion.pose[:2],2)) + self.status)
+            self.status = ''
+            if self.episode > 0:
+                if 'episode' not in self.records.keys(): self.records['episode'] = [self.episode]
+                else: self.records['episode'].append(self.episode)
+                if 'steps' not in self.records.keys(): self.records['steps'] = [self.step_count]
+                else: self.records['episode'].append(self.episode)
+                if 'hit' not in self.records.keys(): self.records['hit'] = [1] if self.status=='hit' else [0]
+                else: self.records['hit'].append(1 if self.status=='hit' else 0)
+                if 'success' not in self.records.keys(): self.records['success'] = [1] if self.status=='success' else [0]
+                else: self.records['success'].append(1 if self.status=='success' else 0)
+                if 'hitfood' not in self.records.keys(): self.records['hitfood'] = [1] if self.status=='hitfood' else [0]
+                else: self.records['hitfood'].append(1 if self.status=='hitfood' else 0)
+                if 'timeout' not in self.records.keys(): self.records['timeout'] = [1] if self.status=='timeout' else [0]
+                else: self.records['timeout'].append(1 if self.status=='timeout' else 0)
+                if 'outbound' not in self.records.keys(): self.records['outbound'] = [1] if self.status=='outbound' else [0]
+                else: self.records['outbound'].append(1 if self.status=='outbound' else 0)
+            else: self.records.clear()
+            self.episode+=1
+        self._episode_ended=False
+        for key, val in zip(kwargs.keys(), kwargs.values()):
+            self.cache[key] = val
+        init_pose = help.spawn_bat(self.cache['mode'], self.cache['phase'])
+        self.locomotion = State(pose=init_pose, dt=1/controlconfig.CHIRP_RATE)
+        self.sensor = Spatializer.Render()
+        self.controller = AvoidApproach()
+        level = self.level if self.cache['max_level']>0 else self.cache['init_level']
+        self.objects[self.objects[:,2]==sensorconfig.OBJECTS_DICT['pole']]=help.spawn_food(self.cache['mode'],
+                                                                                           level, 
+                                                                                           self.cache['difficulty'])
+        #self.objects[0] = np.asarray([(help.MAZE_SIZE/2-help.TUNNEL_WIDTH/4),1.,1])
+        self.echoes = self.sensor.run(pose=self.locomotion.pose, objects=self.objects)
+        self._state = np.asarray(list(self.echoes.values())).reshape(-1,)
+        self.level = self.cache['init_level']
+        self.step_count = 0
+        return ts.restart(self._state)
+
---
+++ Gym/ReactiveSingleFlowerDocking/Helper.py
@@ -0,0 +1,139 @@
+import numpy as np
+import os
+import sys
+import pathlib
+
+
+REPO_NAME = 'FlowerDockingSensorimotorLoopRL'
+REPO_PATH = os.path.abspath(__file__)
+while os.path.basename(REPO_PATH) != REPO_NAME: REPO_PATH = os.path.dirname(REPO_PATH)
+if REPO_PATH not in sys.path: sys.path.append(REPO_PATH)
+from Sensors.BatEcho import Setting as sensorconfig
+from Arena import Builder
+
+from tensorflow import saved_model
+
+HIT_DISTANCE = {'pole': 0.3, 'plant': 0.4}
+OBJECT_SPACING = {'pole': 0.1, 'plant': 0.3}
+MAZE_SIZE = 16.
+TUNNEL_WIDTH = 3.
+JITTER={'xy': 0.1, 'theta': np.pi/18}
+
+
+def collision_check(inview, mode):
+    inview_of_klass = inview[inview[:,2]==sensorconfig.OBJECTS_DICT[mode]][:,:2]
+    if np.sum(inview_of_klass[:,0]<HIT_DISTANCE[mode]) > 0:
+        return True
+    else:
+        return False
+
+
+def get_closest_from_inview(inview, mode):
+    inview_of_klass = inview[inview[:,2]==sensorconfig.OBJECTS_DICT[mode]][:,:2] 
+    argmin = np.argmin(inview_of_klass[:,0])
+    return inview_of_klass[argmin].reshape(-1,)
+
+
+def reward_function(**kwargs):
+    if 'hit' in kwargs.keys():
+        hit = kwargs['hit']
+        if hit == 'plant': return -1
+        if hit == 'pole': 
+            if 'food_polar' in kwargs.keys():
+                food_polar = kwargs['food_polar'].reshape(-1,)[:2]
+                #print(food_polar)
+                if np.abs(food_polar[1]) < 30:
+                    return 2
+                elif np.abs(food_polar[1])<45:
+                    return 1
+                elif np.abs(food_polar[1]) < 90:
+                    return 0.2
+                else: return 0
+    else: return 0
+
+
+def load_policy(checkpoint_dir, agent_id, parent_dir=os.path.dirname(os.path.abspath(__file__))):
+    path = os.path.join(parent_dir, checkpoint_dir, agent_id)
+    return saved_model.load(path)
+
+
+def maze_builder(mode, maze_size=MAZE_SIZE, tunnel_width=TUNNEL_WIDTH):
+    if mode=='box':
+        starts, ends = [],[] 
+        starts.append([-maze_size/2, -maze_size/2])
+        ends.append([maze_size/2, -maze_size/2])
+        starts.append([maze_size/2, -maze_size/2])
+        ends.append([maze_size/2, maze_size/2])
+        starts.append([maze_size/2, maze_size/2])
+        ends.append([-maze_size/2, maze_size/2])
+        starts.append([-maze_size/2, maze_size/2])
+        ends.append([-maze_size/2, -maze_size/2])
+        #
+        starts.append([-(maze_size/2 - tunnel_width), -(maze_size/2 - tunnel_width)])
+        ends.append([(maze_size/2 - tunnel_width), -(maze_size/2 - tunnel_width)])
+        starts.append([(maze_size/2 - tunnel_width), -(maze_size/2 - tunnel_width)])
+        ends.append([(maze_size/2 - tunnel_width), (maze_size/2 - tunnel_width)])
+        starts.append([(maze_size/2 - tunnel_width), (maze_size/2 - tunnel_width)])
+        ends.append([-(maze_size/2 - tunnel_width), (maze_size/2 - tunnel_width)])
+        starts.append([-(maze_size/2 - tunnel_width), (maze_size/2 - tunnel_width)])
+        ends.append([-(maze_size/2 - tunnel_width), -(maze_size/2 - tunnel_width)])
+        #
+        spacings = OBJECT_SPACING['plant']*np.ones(8)
+        maze = Builder.build_walls(starts, ends, spacings)
+    elif mode=='donut':
+        maze = Builder.build_circles(np.zeros((2,2)), [maze_size/2, maze_size/2 - tunnel_width], OBJECT_SPACING['plant']*np.ones(2))
+
+    return np.hstack((maze, sensorconfig.OBJECTS_DICT['plant']*np.ones((len(maze),1))))
+
+
+def spawn_food(mode, level, difficulty=0, maze_size=MAZE_SIZE, tunnel_width=TUNNEL_WIDTH):
+    if mode=='box':
+        xlim_ls_normal=[((maze_size/2-tunnel_width) + OBJECT_SPACING['plant'], maze_size/2 - OBJECT_SPACING['plant']),(-(maze_size/2 - tunnel_width), 0),
+                        (-(maze_size/2 - OBJECT_SPACING['plant']),-(maze_size/2 - tunnel_width + OBJECT_SPACING['plant'])),(0, (maze_size/2 - tunnel_width))]
+        xlim_ls_hard = xlim_ls_normal.copy()
+        xlim_ls_hard[1], xlim_ls_hard[3] = (-(maze_size/2 - OBJECT_SPACING['plant']), 0), (0, maze_size/2 - OBJECT_SPACING['plant'])
+        #
+        ylim_ls_normal=[(0, (maze_size/2 - tunnel_width)),((maze_size/2-tunnel_width) + OBJECT_SPACING['plant'], maze_size/2 - OBJECT_SPACING['plant']),
+                        (0, -(maze_size/2 - tunnel_width)),(-(maze_size/2 - OBJECT_SPACING['plant']), -(maze_size/2-tunnel_width+OBJECT_SPACING['plant']))]
+        ylim_ls_hard = ylim_ls_normal.copy()
+        ylim_ls_hard[0], ylim_ls_hard[2] = (0, maze_size/2 - OBJECT_SPACING['plant']), (0, -(maze_size/2 - OBJECT_SPACING['plant']))
+        #
+        xlim = xlim_ls_normal[int(level%4)] if difficulty==0 else xlim_ls_hard[int(level%4)]
+        ylim = ylim_ls_normal[int(level%4)] if difficulty==0 else ylim_ls_hard[int(level%4)]
+        #
+        food = np.asarray([np.random.uniform(*xlim), np.random.uniform(*ylim), sensorconfig.OBJECTS_DICT['pole']]).reshape(1,3)
+    if mode=='donut':
+        rlim = (maze_size/2 - tunnel_width + OBJECT_SPACING['plant'], maze_size/2 - OBJECT_SPACING['plant'])
+        alim = np.asarray([0, (1+difficulty)*np.pi/9])
+        rho = np.random.uniform(*rlim)
+        phi = np.random.uniform(*Builder.wrap2pi(alim + (level%4)*(np.pi/2)))
+        food = np.asarray([ *Builder.pol2cart(rho,phi), sensorconfig.OBJECTS_DICT['pole']]).reshape(1,3)
+    return food
+
+
+def spawn_bat(mode, phase, maze_size=MAZE_SIZE, tunnel_width=TUNNEL_WIDTH, jitter=True): 
+    if mode=='box':
+        pose_ls = [(maze_size/2-tunnel_width/2,-0.5,np.pi/2),(maze_size/2-tunnel_width/2,-1.0,np.pi/2),
+                   (maze_size/2-tunnel_width/2,-1.5,np.pi/2),(0,-(maze_size/2-tunnel_width/2),0.)]
+        pose = np.asarray([*pose_ls[phase]])
+    if mode=='donut':
+        rho = maze_size/2-tunnel_width/2
+        phi_ls = np.asarray([-1., -2., -3., -9])*(np.pi/18)
+        phi = phi_ls[phase]
+        pose = np.asarray([ *Builder.pol2cart(rho, phi), Builder.wrap2pi(phi + np.pi/2) ])
+    if jitter:
+        pose += np.asarray([JITTER['xy'], JITTER['xy'] , JITTER['theta']])*np.random.uniform(-1,-1,size=3)
+    return pose
+
+
+def out_of_bound(pose, mode, maze_size=MAZE_SIZE, tunnel_width=TUNNEL_WIDTH):
+    x, y = pose[:2]
+    if mode=='box':
+        if (x<0 and x>-(maze_size/2-tunnel_width)) and y>(maze_size/2-tunnel_width): return 1
+        elif x<-(maze_size/2-tunnel_width) and (y>-(maze_size/2-tunnel_width) and y<0.) : return -1
+        else: return 0
+    if mode=='donut':
+        rho, phi = Builder.cart2pol(x,y)
+        if phi > np.pi/2: return 1
+        elif phi < -3*np.pi/4: return -1
+        else: return 0
