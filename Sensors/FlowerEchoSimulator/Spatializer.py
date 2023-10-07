Allow Render to pass on kwargs to other objs, update apps with correct object orient sign

Sensors/FlowerEchoSimulator/Spatializer.py
@@ -366,15 +366,20 @@ class RenderBase:
 
 
 class Render(RenderBase):
-    def __init__(self, mode:AnyStr='compress', cache_dict:Dict=dict(),):
+    def __init__(self, mode:AnyStr='compress', cache_dict:Dict=dict(),
+                 retriever_kwargs:Dict=dict(),
+                 viewer_kwargs:Dict=dict(),
+                 compress_kwargs:Dict=dict(),
+                 cochlea_kwargs:Dict=dict(),
+                 ):
         super().__init__(cache_dict=cache_dict)
         if mode not in ['compress', 'envelope', 'waveform']: 
             raise ValueError('mode must be one of compress, envelope, or waveform')
         self.render_mode = mode
-        self.fetch = OrientationalRetriever()
-        self.viewer = Viewer.FieldOfView()
-        self.compression_filter = Compressing.Subsample()
-        self.cochlea_filter = Cochlea.CochleaFilter()
+        self.fetch = OrientationalRetriever(**retriever_kwargs)
+        self.viewer = Viewer.FieldOfView(**viewer_kwargs)
+        self.compression_filter = Compressing.Subsample(**compress_kwargs)
+        self.cochlea_filter = Cochlea.CochleaFilter(**cochlea_kwargs)
 
     def run(self, bat_pose: ArrayLike, cartesian_objects_matrix: ArrayLike,):
         if type(cartesian_objects_matrix)==list: cartesian_objects_matrix = np.asarray(cartesian_objects_matrix).reshape(-1,4)

apps/VisualizeFlowerEcho.py
@@ -90,10 +90,12 @@ def utest_render_2():
 
     def update(val):
         bat_pose = np.zeros(3).astype(float)
-        bat_pose[2] = Spatializer.wrapToPi(np.radians(sbatorient.val) + init_bat_orientation)
+        bat_pose[2] = Spatializer.wrapToPi(init_bat_orientation- np.radians(sbatorient.val) )
         cartesian_objects_matrix[0,1] = sdistance.val
-        cartesian_objects_matrix[0,2] = Spatializer.wrapToPi(np.radians(sflowerorient.val) + init_flower_orientation)
+        cartesian_objects_matrix[0,2] = Spatializer.wrapToPi(init_flower_orientation - np.radians(sflowerorient.val))
         render.run(bat_pose, cartesian_objects_matrix)
+        #print('bat_theta', np.degrees(render.viewer.filtered_objects_inview_polar[0,1]))
+        #print('flower_theta', np.degrees(render.viewer.filtered_objects_inview_polar[0,2]))
         if render.viewer.collision_status==True:
             bat_arrow.set_color('r')
             bat_pose = np.copy(render.viewer.bat_pose)