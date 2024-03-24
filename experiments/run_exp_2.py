6cd18b5e45c8fd506061fe0f297edf6b668eb38b
Thinh Nguyen <huythinhnguyen@gmail.com>
2024-03-24 18:27:01 -0400

    fix bug exp2

--- experiments/run_exp_2.py
+++ experiments/run_exp_2.py
@@ -38,6 +38,8 @@ INIT_CONFIG = {
 }
 
 FLOWER_COLLISION_RADIUS: float = 0.22
+FLOWER_OPENING_ANGULAR_RANGE: Tuple[float] = (-4*np.pi/18, 4*np.pi/18)
+BAT_FACING_ANGULAR_RANGE: Tuple[float] = (-7*np.pi/18, 7*np.pi/18)
 ARENA_LIM = {'x': (-1., 4.), 'y': (-2.5, 2.5)}
 N_TRIAL: int = 200
 
@@ -287,81 +289,3 @@ def main():
 
 if __name__=='__main__':
     main()
-
-
-
-# def test1():
-#     # make a interactive widget to plot arrows showing the bat and flower pose.
-#     import matplotlib.pyplot as plt
-#     from matplotlib.patches import FancyArrowPatch
-#     from matplotlib.widgets import Slider
-#     from matplotlib import animation
-#     from matplotlib.animation import FuncAnimation
-#     from matplotlib import patches
-#     from matplotlib import transforms
-#     from matplotlib import cm
-
-#     fig, ax = plt.subplots()
-#     ax.set_xlim(-1, 2)
-#     ax.set_ylim(-1, 2)
-#     ax.set_aspect('equal')
-#     ax.grid(True)
-#     ax.set_xlabel('x')
-#     ax.set_ylabel('y')
-#     ax.set_title('Bat and Flower Pose')
-
-#     bat_pose = np.array([0., 0., 0.])
-#     flower_pose = np.array([1., 0., 0.])
-#     bat_arrow = FancyArrowPatch((bat_pose[0], bat_pose[1]), (bat_pose[0]+0.3*np.cos(bat_pose[2]), bat_pose[1]+0.3*np.sin(bat_pose[2])),
-#                                 arrowstyle='simple', mutation_scale=20, color='blue')
-#     flower_arrow = FancyArrowPatch((flower_pose[0], flower_pose[1]), (flower_pose[0]+0.3*np.cos(flower_pose[2]), flower_pose[1]+0.3*np.sin(flower_pose[2])),
-#                                       arrowstyle='simple', mutation_scale=20, color='red')
-#     ax.add_patch(bat_arrow)
-#     ax.add_patch(flower_arrow)
-#     fig.subplots_adjust(bottom=0.3)
-#     axcolor = 'lightgoldenrodyellow'
-#     ax_x = plt.axes([0.25, 0.01, 0.65, 0.03], facecolor=axcolor)
-#     ax_y = plt.axes([0.25, 0.06, 0.65, 0.03], facecolor=axcolor)
-#     ax_theta = plt.axes([0.25, 0.11, 0.65, 0.03], facecolor=axcolor)
-#     ax_flower_theta = plt.axes([0.25, 0.16, 0.65, 0.03], facecolor=axcolor)
-    
-#     # add a changeable textbox show the sign of bat pose [2]
-
-#     status_text = ax.text(2.2, 1.8, 'STATUS: ??', ha='left', va='center', fontsize=14)
-#     distance_text = ax.text(2.2, 1.5, 'd = ??', ha='left', va='center', fontsize=14)
-#     aoa_text = ax.text(2.2, 1.2, 'aoa = ??', ha='left', va='center', fontsize=14)
-#     azi_text = ax.text(2.2, 0.8, 'azi = ??', ha='left', va='center', fontsize=14)
-
-#     s_x = Slider(ax_x, 'x', ARENA_LIM['x'][0], ARENA_LIM['x'][1], valinit=bat_pose[0])
-#     s_y = Slider(ax_y, 'y', ARENA_LIM['y'][0], ARENA_LIM['y'][1], valinit=bat_pose[1])
-#     s_theta = Slider(ax_theta, 'theta', -np.pi, np.pi, valinit=bat_pose[2])
-#     s_flower_theta = Slider(ax_flower_theta, 'flower_theta', -np.pi, np.pi, valinit=flower_pose[2])
-
-#     def update(val):
-#         bat_pose[0] = s_x.val
-#         bat_pose[1] = s_y.val
-#         bat_pose[2] = s_theta.val
-#         flower_pose[2] = s_flower_theta.val
-#         bat_arrow.set_positions((bat_pose[0], bat_pose[1]), (bat_pose[0]+0.3*np.cos(bat_pose[2]), bat_pose[1]+0.3*np.sin(bat_pose[2])))
-#         flower_arrow.set_positions((flower_pose[0], flower_pose[1]), (flower_pose[0]+0.3*np.cos(flower_pose[2]), flower_pose[1]+0.3*np.sin(flower_pose[2])))
-#         fig.canvas.draw_idle()
-#         distance = np.linalg.norm(bat_pose[:2]-flower_pose[:2])
-#         distance_text.set_text(f'd = {np.round(distance, 3)}')
-#         aoa = get_angle_of_arrival(bat_pose, flower_pose)
-#         aoa_text.set_text(f'aoa = {np.round(np.degrees(aoa), 1)}')
-#         azi = get_azimuth_of_flower(bat_pose, flower_pose)
-#         azi_text.set_text(f'azi = {np.round(np.degrees(azi), 1)}')
-#         if collision_check(bat_pose, np.array([flower_pose])):
-#             if check_docking(bat_pose, flower_pose):
-#                 status_text.set_text('STATUS: Dock')
-#             else:
-#                 status_text.set_text('STATUS: Hit')
-#         else: status_text.set_text('STATUS: OK')
-
-
-#     s_x.on_changed(update)
-#     s_y.on_changed(update)
-#     s_theta.on_changed(update)
-#     s_flower_theta.on_changed(update)
-
-#     plt.show()
